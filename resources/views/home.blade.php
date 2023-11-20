b70270dda0319f861a881a974811da224c0d83f4
Alethepata <alessia.patane@gmail.com>
2023-11-20 15:46:18 +0100

    [Add] movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,20 @@
 @section('content')
   <div class="container">
     <h1>Film</h1>
+    <div class="card_container d-flex flex-wrap">
+        @foreach ($movies as $movie)
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+
+                    <h3>{{$movie->title}}</h3>
+                    <h6>{{$movie->original_title}}</h6>
+                    <p>{{$movie->nationality}}</p>
+                    <p>{{$movie->date}}</p>
+                    <p>{{$movie->vote}}</p>
+
+                </div>
+            </div>
+    @endforeach
+    </div>
   </div>
 @endsection
