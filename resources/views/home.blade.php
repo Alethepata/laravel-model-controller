606cba986019071e421134602e91d3920e6a4e6f
Alethepata <alessia.patane@gmail.com>
2023-11-20 16:06:06 +0100

    [Add] img

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,12 @@
 @section('content')
   <div class="container">
     <h1>Film</h1>
-    <div class="card_container d-flex flex-wrap">
+    <div class="card_container d-flex flex-wrap justify-content-center ">
         @foreach ($movies as $movie)
             <div class="card" style="width: 18rem;">
+
+                <img src="{{$movie->image}}" class="card-img-top" alt="{{$movie->title}}">
+
                 <div class="card-body">
 
                     <h3>{{$movie->title}}</h3>
