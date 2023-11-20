f300565c384005da5befabfd71ab27d9c0701527
Alethepata <alessia.patane@gmail.com>
2023-11-20 16:42:27 +0100

    [edit] text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,17 @@
     <h1>Film</h1>
     <div class="card_container d-flex flex-wrap justify-content-center mt-5">
         @foreach ($movies as $movie)
-            <div class="card mx-3 my-4" style="width: 18rem;">
+            <div class="card mx-3 my-5" style="width: 18rem;">
 
                 <img class="rounded-top-4" src="{{$movie->image}}" class="card-img-top" alt="{{$movie->title}}">
 
                 <div class="card-body rounded-bottom-4">
 
-                    <h3>{{$movie->title}}</h3>
-                    <h6>{{$movie->original_title}}</h6>
-                    <p>{{$movie->nationality}}</p>
-                    <p>{{$movie->date}}</p>
-                    <p>{{$movie->vote}}</p>
+                    <h3 class="fw-semibold">{{$movie->title}}</h3>
+                    <h6 class="fst-italic">{{$movie->original_title}}</h6>
+                    <p class="text-capitalize">{{$movie->nationality}}</p>
+                    <p class="fs-6 fst-italic">{{$movie->date}}</p>
+                    <p>{{$movie->vote}} <span class="text-warning">&starf;</span></p>
 
                 </div>
             </div>
