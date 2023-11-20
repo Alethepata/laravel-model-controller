cbbeeb704f016cd4f23487cc911cadaef7f83537
Alethepata <alessia.patane@gmail.com>
2023-11-20 16:35:00 +0100

    [Add] card margin/container margin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,15 @@
 @extends('layout.main')
 
 @section('content')
-  <div class="container">
+  <div class="container my-5">
     <h1>Film</h1>
-    <div class="card_container d-flex flex-wrap justify-content-center ">
+    <div class="card_container d-flex flex-wrap justify-content-center mt-5">
         @foreach ($movies as $movie)
-            <div class="card" style="width: 18rem;">
+            <div class="card mx-3 my-4" style="width: 18rem;">
 
-                <img src="{{$movie->image}}" class="card-img-top" alt="{{$movie->title}}">
+                <img class="rounded-top-4" src="{{$movie->image}}" class="card-img-top" alt="{{$movie->title}}">
 
-                <div class="card-body">
+                <div class="card-body rounded-bottom-4">
 
                     <h3>{{$movie->title}}</h3>
                     <h6>{{$movie->original_title}}</h6>
