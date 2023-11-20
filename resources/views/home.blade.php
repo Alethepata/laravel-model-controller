092d0783c38c5ac2e53e02f7c59dda68a5f6372f
Alethepata <alessia.patane@gmail.com>
2023-11-20 15:32:00 +0100

    [Add] container

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Film</title>
-    @vite('resources/js/app.js')
-</head>
-<body>
+@extends('layout.main')
 
-</body>
-</html>
+@section('content')
+  <div class="container">
+    <h1>Film</h1>
+  </div>
+@endsection
